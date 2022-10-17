5e7bc9d26abe27a5ba57aac056f606e1cb38bdc7
Anamika Sawhney <anamika.sawhney22@gmail.com>
2022-10-17 14:10:15 +0530

    Constructors_StaticVariables

---
+++ Constructors_StaticVariables/Employee.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors_StaticVariables
+{
+    class Employee
+    {
+        int id;
+        string name;
+        static string dept;
+        public const string companyName="CTS";
+        int salary;
+        static int count;
+        // Def Cons
+          public Employee() {}
+
+        // Para Cons
+        public Employee(string name)
+        {
+            //Employee employee = new Employee();
+            this.name = name;
+            Console.WriteLine("Enter ID");
+            id = Int16.Parse(Console.ReadLine());
+           
+            //Console.WriteLine("Enter dept");
+            //dept = Console.ReadLine();
+            //Console.WriteLine("Enter companyName");
+            //companyName = Console.ReadLine();
+            Console.WriteLine("Enter Salary");
+            salary = Int32.Parse(Console.ReadLine());
+            count++;
+
+        }
+
+
+        // Para Cons
+        public Employee(int id, string name)
+        {
+            this.name = name;
+            this.id = id;
+            //Console.WriteLine("Enter dept");
+            //dept = Console.ReadLine();
+            //Console.WriteLine("Enter companyName");
+            //companyName = Console.ReadLine();
+            Console.WriteLine("Enter Salary");
+            salary = Int32.Parse(Console.ReadLine());
+            count++;
+
+        }
+
+        // Para Cons
+        public Employee(int id, string name  , int salary)
+        {
+            this.name = name;
+            this.id = id;
+            //dept = "HR";
+            //this.companyName = companyName;
+            this.salary = salary;
+            count++;
+
+        }
+       
+        // Copy Cons
+        public Employee (Employee employee)
+        {
+            Console.WriteLine("Enter ID");
+            id = Int16.Parse(Console.ReadLine());
+            this.name = employee.name;
+            //dept = "HR";
+            //this.companyName = employee.companyName;
+            this.salary = employee.salary;
+        }
+        // Static Cons
+        // are only 1 in a class
+        // They are wothout any access specifier
+        // It is paramterless
+        static Employee()
+        {
+            dept = "HR";
+
+        }
+        public void GetDetails()
+        {
+            Console.WriteLine("Enter ID");
+            id = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            //Console.WriteLine("Enter dept");
+            //dept = Console.ReadLine();
+            //Console.WriteLine("Enter companyName");
+            //companyName = Console.ReadLine();
+            Console.WriteLine("Enter Salary");
+            salary = Int32.Parse(Console.ReadLine());
+            count++;
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Id is {id}\nName is {name}\nSalary is {salary}");
+            Console.WriteLine($"No. of Employees are {count}");
+        }
+        public static void DisplayDept()
+        {
+            //dept = "HR";
+            Console.WriteLine("Dept is " + dept) ;
+        }
+    }
+}
---
+++ Constructors_StaticVariables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Constructors_StaticVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee.dept = "HR";
+            //Console.WriteLine("Dept is " + Employee.dept);
+            Console.WriteLine("Company Name is " + Employee.companyName);
+            Employee.DisplayDept();
+            // This will invoke cons > Def cons
+            Employee employee = new Employee();
+            employee.GetDetails();
+            employee.DisplayDetails();
+
+            // This will invoke cons > Para cons
+            Employee emp2 = new Employee("Ajay");
+            emp2.DisplayDetails();
+
+            // This will invoke cons > Para cons
+            Employee emp3 = new Employee(101, "Lalit");
+
+            emp3.DisplayDetails();
+
+            // This will invoke cons > Para cons
+            Employee emp4 = new Employee(102, "Karan", 23000);
+            emp4.DisplayDetails();
+
+            // This will invoke cons > Copy cons
+            Employee emp5 = new Employee(emp2);
+
+        }
+    }
+}
